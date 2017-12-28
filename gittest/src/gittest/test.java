aeb06c255988f542ca273eab7692b90b9d95bb49
user <user@user-PC18>
2017-12-28 16:02:59 +0900

    최종

--- gittest/src/gittest/test.java
+++ gittest/src/gittest/test.java
@@ -4,28 +4,10 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
-		System.out.println("난 재우!!");
+		System.out.println("난 제우쓰");
+		System.out.println("난 블링블링영동쓰");
+		System.out.println("난 현서쓰");
 
-
-		System.out.println("나 현서!!");
-		System.out.println("나 현서!!");
-		System.out.println("이렇게하는거야");
-		System.out.println("난 블링블링영동!");
-		System.out.println("난 블현서현서현서동!");
-
-		System.out.println("난 재우라구해 꽃미남이지!!");
-
-		System.out.println("난 블링블링영동!");
-
-		System.out.println("난 재우라구해 꽃미남이지!!");
-System.out.println("아니 ㅓㅈ저ㅐㅑㅈㅁㄷ사ㅐㅑ");
-		System.out.println("난 블링블링영동!");
-		System.out.println("현서더티워크트리");
-		System.out.println("아니 ㅓㅈ저ㅐㅑㅈㅁㄷ사ㅐㅑ");
-		System.out.println("난 블링블링영동!");
-		System.out.println("난 블링블링영동!");
-		System.out.println("난 블링블링영동!");
 	}
 
 }
