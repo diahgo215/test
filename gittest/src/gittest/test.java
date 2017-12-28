d657eabe1e4ba81da6a608793357b7bc656cdbf3
user <user@user-PC18>
2017-12-28 15:42:49 +0900

    456

--- gittest/src/gittest/test.java
+++ gittest/src/gittest/test.java
@@ -6,7 +6,8 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("난 재우!!");
+		System.out.println("난 재우라구해 꽃미남이지!!");
+
 		System.out.println("난 블링블링영동!");
 	}
-
 }
