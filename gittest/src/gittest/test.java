5f7ca0c49d65116a5d13ad3fbd9f75dc9f402967
user <user@user-PC18>
2017-12-28 16:05:48 +0900

    qwe

--- gittest/src/gittest/test.java
+++ gittest/src/gittest/test.java
@@ -1,14 +1,9 @@
 package gittest;
-
 public class test {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("난 제우쓰");
 		System.out.println("난 블링블링영동쓰");
 		System.out.println("난 현서쓰");
-
 	}
-
 }
-
